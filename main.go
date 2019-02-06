cce6b7dd3301c0d32809015e33609c92ece284aa
astaphobia <ariasta.ketut@icloud.com>
2019-02-06 18:41:13 +0800

    init go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprint(w, "Hello Guys")
+}
